297478d9f1df6c38708668a9844a42f11caf1ced
Ken-SP4 <yongjian.ouyang@outlook.com>
2018-12-21 14:50:06 -0600

    Updated run book

--- runbook.sh
+++ runbook.sh
@@ -1,17 +1,33 @@
+## Stop/Remove Container
 sudo docker stop data-lab
 sudo docker rm data-lab
 
+## Remove Image
 sudo docker rmi bigdataplot/jupyter-spark-lab:1.31
 
-sudo docker build -t bigdataplot/jupyter-spark-lab:1.31 .
+## Build from Dockerfile
+sudo docker build -t bigdataplot/jupyter-spark-lab:1.33 .
 
+## Check Docker Logs
 sudo docker logs data-lab
 
-sudo push bigdataplot/jupyter-spark-lab:1.31
+## Login and Push a Image
+sudo docker login --username bigdataplot
+sudo docker push bigdataplot/jupyter-spark-lab:1.33
 
+
+## Setup Host Environments
+sudo addgroup docker && \
+sudo newgrp docker
+
+sudo mkdir -p /apps/datahub && \
+sudo chmod -R 770 /apps/datahub && \
+sudo chown -R root:docker /apps/datahub
+
+## Bring up the Jupyter-Spark-Lab (Modify ports if necessary)
 sudo docker run --name data-lab \
     --detach \
     --restart always\
     --publish 8888:8888 \
     --volume /apps/datahub:/apps/datahub \
-    bigdataplot/jupyter-spark-lab:1.31
+    bigdataplot/jupyter-spark-lab
